10db7cff67d916b231842ae2f125fb8351b0a4ac
nak-244 <t09024656860@gmail.com>
2019-05-09 18:30:38 +0900

    Update search.js

--- docs/js/search.js
+++ docs/js/search.js
@@ -8,9 +8,6 @@ $(function() {
                     var srh_jobtype_child_param = $('.srh_jobtype_child_param:checked').map(function() {
                       return $(this).val();
                     }).get();
-                    var srh_room_type_param = $('.srh_room_type_param:checked').map(function() {
-                      return $(this).val();
-                    }).get();
 
                     $('#test').prop('href', href + "?fuseaction=job.joblist&srh_ken_param=" + srh_ken_param + "&srh_city_param=" + srh_city_param + "&srh_jobtype_child_param=" + srh_jobtype_child_param + "&srh_shift_param=" + srh_shift_param + "&srh_room_type_param=" + srh_room_type_param);
                   });

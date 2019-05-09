f758af5a1238562c4b9b2b80d5cb4822f2e87a30
nak-244 <t09024656860@gmail.com>
2019-05-09 18:36:32 +0900

    up

--- docs/js/search.js
+++ docs/js/search.js
@@ -1,10 +1,7 @@
 $(function() {
                   var href = "https://olp-cat.jp/index.cfm";
-                  $('#srh_ken_param,.srh_city_param,.srh_jobtype_child_param,.srh_shift_param,.srh_room_type_param').on('change', function() {
+                  $('#srh_ken_param,.srh_jobtype_child_param').on('change', function() {
                     var srh_ken_param = $('#srh_ken_param').val();
-                    var srh_city_param = $('.srh_city_param:checked').map(function() {
-                      return $(this).val();
-                    }).get();
                     var srh_jobtype_child_param = $('.srh_jobtype_child_param:checked').map(function() {
                       return $(this).val();
                     }).get();

341c622371828a895c043e27708aa932a4f9d4ee
nak-244 <t09024656860@gmail.com>
2019-05-10 12:27:23 +0900

    Update search.js

--- docs/js/search.js
+++ docs/js/search.js
@@ -1,9 +1,10 @@
 $(function() {
                   var href = "https://olp-cat.jp/index.cfm";
-                  $('#srh_ken_param,#srh_year_money_param').on('change', function() {
+                  $('#srh_ken_param,#srh_year_money_param,#srh_jobtype_param').on('change', function() {
                     var srh_ken_param = $('#srh_ken_param').val();
                     var srh_year_money_param = $('#srh_year_money_param').val();
+                    var srh_jobtype_param = $('#srh_jobtype_param').val();
 
-                    $('#test').prop('href', href + "?fuseaction=job.joblist&srh_ken_param=" + srh_ken_param + "&srh_year_money_param=" + srh_year_money_param);
+                    $('#test').prop('href', href + "?fuseaction=job.joblist&srh_ken_param=" + srh_ken_param + "&srh_year_money_param=" + srh_year_money_param + "&srh_jobtype_param=" + srh_jobtype_param);
                   });
                 });
